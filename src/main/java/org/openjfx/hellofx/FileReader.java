10cbd05649435d8cdfa823ce75f364d3b3eb0ef0
toqvist <tobias-ahlqvist@outlook.com>
2022-09-13 20:32:40 +0200

    Build and show table implemented

--- src/main/java/org/openjfx/hellofx/FileReader.java
+++ src/main/java/org/openjfx/hellofx/FileReader.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -26,9 +25,10 @@ public class FileReader {
 				break;
 			case "json":
 				break;
-			case "xml":
-				break;
+			// case "xml":
+			// 	break;
 			default:
+				System.out.println("Filetype not supported");
 				break;
 
 		}
@@ -48,42 +48,38 @@ public class FileReader {
 	 * @throws FileNotFoundException
 	 * @throws IOException
 	 */
-	public static LinkedHashMap<String, ArrayList<String>> readCSV(File file) throws FileNotFoundException, IOException {
+	public static LinkedHashMap<String, ArrayList<String>> readCSV(File file)
+			throws FileNotFoundException, IOException {
 
-		// if (file.exists()) {
-		// 	System.out.println(
-		// 			"FILE FOUND!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-		// }
-
-
-		//Create map with an associated list for each label
+		// Create map with an associated list for each label
 		LinkedHashMap<String, ArrayList<String>> map = new LinkedHashMap<String, ArrayList<String>>();
 
 		try (Scanner scanner = new Scanner(file)) {
 
-			//Get the first line, initialize a key for each label and pair it with a new list.
+			// Get the first line, initialize a key for each label and pair it with a new
+			// list.
 			String[] labels = scanner.nextLine().split(",");
 
 			for (String label : labels) {
 				map.put(label, new ArrayList<String>());
 			}
 
-			//Populate the lists in the map with column values.
+			// Populate the lists in the map with column values.
 			while (scanner.hasNextLine()) {
 
-					String[] columns = scanner.nextLine().split(",");
+				String[] columns = scanner.nextLine().split(",");
 
-					//For each entry(list) in the map, add the column
-					int i = 0;
-					for (Map.Entry<String, ArrayList<String>> entry : map.entrySet()) {
-						//Get lists from map, add a column value to each.
-						try {
-							entry.getValue().add(columns[i]);
-						} catch (ArrayIndexOutOfBoundsException e) {
-							entry.getValue().add("");
-						}
-							i++;
+				// For each entry(list) in the map, add the column
+				int i = 0;
+				for (Map.Entry<String, ArrayList<String>> entry : map.entrySet()) {
+					// Get lists from map, add a column-value to each.
+					try {
+						entry.getValue().add(columns[i]);
+					} catch (ArrayIndexOutOfBoundsException e) {
+						entry.getValue().add("");
 					}
+					i++;
+				}
 			}
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
--- src/main/java/org/openjfx/hellofx/PrimaryController.java
+++ src/main/java/org/openjfx/hellofx/PrimaryController.java
@@ -2,30 +2,106 @@ package org.openjfx.hellofx;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.Map;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-
-
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.MapValueFactory;
 import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 public class PrimaryController {
 
     @FXML
-    private void readFile() throws IOException {
+    private void readFile(ActionEvent event) throws IOException {
 
-        //Initialize FileChooser, filter by extensions .csv and .json
+        // Initialize FileChooser, filter by extensions .csv and .json
         FileChooser fileChooser = new FileChooser();
         fileChooser.getExtensionFilters().addAll(
                 new FileChooser.ExtensionFilter("All Files", "*.*"),
                 new FileChooser.ExtensionFilter("CSV", "*.csv"),
                 new FileChooser.ExtensionFilter("JSON", "*.json"),
-                new FileChooser.ExtensionFilter("XML", "*.xml"));	
+                new FileChooser.ExtensionFilter("XML", "*.xml"));
 
         File file = fileChooser.showOpenDialog(null);
-        LinkedHashMap map = FileReader.read(file);
+        LinkedHashMap<String, ArrayList<String>> map = FileReader.read(file);
 
-        // System.out.println(file);
+        buildTable(map, event);
+
     }
 
+    public static void buildTable(LinkedHashMap<String, ArrayList<String>> map, ActionEvent event) {
+
+        TableView tableView = new TableView();
+
+        tableView.setEditable(true);
+
+        // Create a map as a data item for the table columns
+        ObservableList<Map<String, Object>> items = FXCollections.<Map<String, Object>>observableArrayList();
+
+        // For each key/value pair in map, create a column with the key as the label
+        for (Map.Entry<String, ArrayList<String>> entry : map.entrySet()) {
+
+            TableColumn<Map, String> column = new TableColumn<>(entry.getKey());
+            column.setCellValueFactory(new MapValueFactory<>(entry.getKey()));
+            tableView.getColumns().add(column);
+
+        }
+
+    
+        ArrayList<String> firstList = map.entrySet().iterator().next().getValue();
+
+        //For each item (row)
+        for (int i = 0; i < firstList.size(); i++) {
+
+            Map<String, Object> item = new HashMap<>();
+
+            //Populate with values from each column
+            int j = 0;
+            for (Map.Entry<String, ArrayList<String>> entry : map.entrySet()) {
+
+                ArrayList<String> list = entry.getValue();                
+                item.put(entry.getKey(), list.get(j+i));
+
+            }
+            j++;
+            items.add(item);
+        }
+
+        // Add the items to the table
+        tableView.getItems().addAll(items);
+
+        // Get the stage from the event
+        Node node = (Node) event.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+
+        // Group and add objects to the scene
+        Group root = new Group(tableView);
+        Scene scene = new Scene(root, 800, 1200);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void sysout(LinkedHashMap<String, ArrayList<String>> map) {
+        for (Map.Entry<String, ArrayList<String>> entry : map.entrySet()) {
+
+            System.out.println(entry.getKey());
+            // entry.getValue();
+
+            for (String element : entry.getValue()) {
+                System.out.println(element);
+            }
+        }
+    }
 }
